c6d611eace2116864fd660944d740b555b0c10d0
MaxSpasov <114147361+MaxSpasov@users.noreply.github.com>
2023-12-10 23:11:13 +0200

    Changes

--- src/index.js
+++ src/index.js
@@ -26,6 +26,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(auth);
 app.use(routes);
-app.use(errorHandler);
+//app.use(errorHandler);
 
-app.listen(3000, console.log('Server is listening on port 3000...'));
+app.listen(5000, console.log('Server is listening on port 5000...'));
